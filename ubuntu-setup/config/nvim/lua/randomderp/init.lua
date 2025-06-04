594e00068afeb07fca465d0da1dbf71f2c3087c9
niko junttila <nikosamulijunttila@gmail.com>
2025-06-04 10:26:43 +0300

    nvim fixes

--- ubuntu-setup/config/nvim/lua/randomderp/init.lua
+++
@@ -1,9 +0,0 @@
-require("randomderp.remap")
-require("randomderp.lazy")
-require("randomderp.colorscheme")
-require("randomderp.treesitter")
-require("randomderp.fugitive")
-require("randomderp.harpoon")
-require("randomderp.mason")
-
-print("hello from randomderp")
