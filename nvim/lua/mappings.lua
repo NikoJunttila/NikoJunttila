0702d9dfef5fee7d3e6b9c2c66cad92e57f5c3ef
Niko Junttila <nikosamulijunttila@gmail.com>
2026-01-28 08:25:27 +0200

    Refactor and add key mappings in mappings.lua
    Refactor key mappings for normal, insert, and visual modes. Added custom mappings for Go error check and LSP restart.

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,33 +1,19 @@
 require "nvchad.mappings"
 
 -- add yours here
-
 local map = vim.keymap.set
 
+-- Normal mode (n)
+-- Default mode — for navigation and commands, not typing text.
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
---
 map("n", "ö", "$", { desc = "Jump to end of line" })
 map("n", "ä", "0", { desc = "Jump to start of line" })
 
-map("v", "ö", "$", { desc = "Jump to end of line" })
-map("v", "ä", "0", { desc = "Jump to start of line" })
-
 map("n", "m", ":m +1 <CR>", { desc = "Move down" })
 map("n", "n", ":m -2 <CR>", { desc = "Move Up" })
-map("v", "n", ":m -2 <CR>", { desc = "Move Up" })
 
--- Additional custom mappings
-map("n", "<leader>ee", function()
-  vim.api.nvim_put({ "if err != nil {", "  return err", "}" }, "", true, true)
-end, { desc = "Insert Go error check" })
-
-map("i", "<C-h>", "<Left>")
-map("i", "<C-j>", "<Down>", { desc = "Move down in insert mode" })
-map("i", "<C-k>", "<Up>", { desc = "Move up in insert mode" })
-map("i", "<C-l>", "<Right>", { desc = "Move right in insert mode" })
+map("n", "<leader>lr", ":LspRestart <CR>", { desc = "Reset lsp" })
 
 -- Buffer management mappings
 map("n", "<leader>bx", function()
@@ -40,3 +26,22 @@ end, { desc = "Close all buffers (safe)" })
 map("n", "<leader>bo", function()
   vim.cmd "%bd|e#|bd#"
 end, { desc = "Close all buffers except current" })
+
+-- Additional custom mappings
+map("n", "<leader>ee", function()
+  vim.api.nvim_put({ "if err != nil {", "  return err", "}" }, "", true, true)
+end, { desc = "Insert Go error check" })
+
+-- Insert mode (i)
+-- For typing text, like a normal editor.
+map("i", "jk", "<ESC>")
+map("i", "<C-h>", "<Left>")
+map("i", "<C-j>", "<Down>", { desc = "Move down in insert mode" })
+map("i", "<C-k>", "<Up>", { desc = "Move up in insert mode" })
+map("i", "<C-l>", "<Right>", { desc = "Move right in insert mode" })
+
+-- Visual mode (v)
+-- For selecting text.
+map("v", "n", ":m -2 <CR>", { desc = "Move Up" })
+map("v", "ö", "$", { desc = "Jump to end of line" })
+map("v", "ä", "0", { desc = "Jump to start of line" })
