85917156100bfbc186883f7b8a71d2f8c6ca7606
nikojunttila <nikosamulijunttila@gmail.com>
2026-02-06 10:19:18 +0200

    quick

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -23,10 +23,6 @@ return {
       },
       scroll = {
         enabled = false,
-        animate = {
-          duration = { step = 15, total = 250 },
-          easing = "linear",
-        },
       },
     },
     keys = {
