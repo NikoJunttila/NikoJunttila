571008c00b9ff06121c3205585a4c73dd2ec4089
niko.junttila <niko.junttila@hur.fi>
2026-03-09 11:40:15 +0200

    img nvim fix

--- nvim/lua/plugins/mermaid.lua
+++ nvim/lua/plugins/mermaid.lua
@@ -2,6 +2,16 @@ return {
   {
     "3rd/image.nvim",
     build = false,
+
+    -- load for markdown/neorg usage
+    ft = { "markdown", "vimwiki", "norg" },
+
+    -- and also load early when opening an image file directly
+    event = {
+      { event = "BufReadPre", pattern = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" } },
+      { event = "BufNewFile", pattern = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" } },
+    },
+
     opts = {
       backend = "kitty",
       processor = "magick_cli",
@@ -15,20 +25,26 @@ return {
           clear_in_insert_mode = false,
           download_remote_images = true,
           only_render_image_at_cursor = false,
-          only_render_image_at_cursor_mode = "popup", -- or "inline"
-          floating_windows = false,                   -- if true, images will be rendered in floating markdown windows
-          filetypes = { "markdown", "vimwiki" },      -- markdown extensions (ie. quarto) can go here
+          only_render_image_at_cursor_mode = "popup",
+          floating_windows = false,
+          filetypes = { "markdown", "vimwiki" },
+        },
+        neorg = {
+          enabled = true,
+          filetypes = { "norg" },
         },
       },
 
       window_overlap_clear_enabled = true,
+
+      -- this still needs to be here
+      hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" },
     },
-    hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif" }, -- render image files as images when opened
   },
 
   {
     "3rd/diagram.nvim",
-    ft = { "markdown", "norg" }, -- ✅ load only for these
+    ft = { "markdown", "norg" },
     dependencies = { "3rd/image.nvim" },
     opts = {
       events = {
